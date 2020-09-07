e6855cc0cdae58ec6392b3304e6afa880b7d6b25
Andre Gois <andre.s.gois3@gmail.com>
2020-09-07 12:03:54 -0300

    alterado o nome do banco

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 //mongodb+srv://andregois:<password>@blogapp-prod.kwfgn.mongodb.net/<dbname>?retryWrites=true&w=majority
 
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoUri: "mongodb+srv://andregois:05042010@blogapp-prod.kwfgn.mongodb.net/<dbname>?retryWrites=true&w=majority"}
+    module.exports = {mongoUri: "mongodb+srv://andregois:05042010@blogapp-prod.kwfgn.mongodb.net/blogapp-prod?retryWrites=true&w=majority"}
 }else{
     module.exports = {mongoUri: "mongodb://localhost/blogapp"}
 }
